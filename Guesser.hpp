01f170ee9cf37a92ad842931ccede1b24418ebde
Michael-Lemberger <44966756+michael-lemberger@users.noreply.github.com>
2019-04-28 16:36:25 +0300

    Update Guesser.hpp

--- Guesser.hpp
+++ Guesser.hpp
@@ -7,8 +7,8 @@ namespace bullpgia {
 	class Guesser {
 		public:
 			uint length;
-			virtual std::string guess(){return NULL;}=0;
-			virtual void startNewGame(uint theLength){}=0;
-			virtual void learn(std::string){}=0;
+			virtual std::string guess(){return NULL;};
+			virtual void startNewGame(uint theLength){};
+			virtual void learn(std::string){};
  	};
 }
