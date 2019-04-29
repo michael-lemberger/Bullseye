9f44b0e9010ec42d9e578b0f933e610b8bd7c299
Liron Arad <lironarad1@gmail.com>
2019-04-29 22:24:18 +0300

    Update SmartGuesser.hpp

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -1,21 +1,16 @@
 #pragma once
 #include "Guesser.hpp"
 #include "calculate.hpp"
-#include <unordered_set>
-
 
 /**
  * ConstantGuesser is a guesser that always guesses the same string. 
  */
 class SmartGuesser: public bullpgia::Guesser {
-	private:
-		std::unordered_set<std::string> myset;
-		std::string lastGuess;
 
 	public:
 		std::string guess() override;
-		void startNewGame(uint theLength) override;
-		void learn(std::string response) override;
+		void startNewGame(uint size) override;
+		void learn(std::string s) override;
 };
 
-std::string numToGuess(int num, uint length);
+
