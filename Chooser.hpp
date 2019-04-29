b42f93621ef8e1dfc7774dabb5cc2f08cc20a8e9
Liron Arad <lironarad1@gmail.com>
2019-04-29 22:32:48 +0300

    Update Chooser.hpp

--- Chooser.hpp
+++ Chooser.hpp
@@ -6,6 +6,6 @@ namespace bullpgia {
 	class Chooser {
 		public:
 			uint length;
-			virtual std::string choose(uint length){return NULL;};
+			virtual std::string choose(uint){return NULL;};
 	};
 }
