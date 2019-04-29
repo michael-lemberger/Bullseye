7e48b2f74825b53eceef4edb498966e242693408
Liron Arad <lironarad1@gmail.com>
2019-04-29 22:37:57 +0300

    Update Guesser.hpp

--- Guesser.hpp
+++ Guesser.hpp
@@ -8,7 +8,7 @@ namespace bullpgia {
 		public:
 			uint length;
 			virtual std::string guess(){return NULL;};
-			virtual void startNewGame(uint size){};
+			virtual void startNewGame(uint){};
 			virtual void learn(std::string){};
  	};
 }
