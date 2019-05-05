2683b1c7b4791034d878ca3d1c49415491481ae8
Michael-Lemberger <44966756+michael-lemberger@users.noreply.github.com>
2019-05-05 17:04:13 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -6,60 +6,59 @@ using std::string, std::to_string;
 using namespace std;
 namespace bullpgia {
 	const string calculateBullAndPgia(const string choosen, const string guess){
-		// if(choosen.length() != guess.length()){//if the length are not equals
-  //       return "0,0";
-		// }
+		if(choosen.length() != guess.length()){//if the length are not equals
+        return "0,0";
+		}
 
-  //   else{
-  //   	check(choosen,guess);
-  //       uint bull = 0;
-  //       uint pgia = 0;
- 	// 	char bulled;
- 	// 	bool count[guess.length()];
- 	// 	for(uint i=0;i<guess.length();i++){
- 	// 		count[i]=false;
- 	// 	}
+    else{
+    	check(choosen,guess);
+        uint bull = 0;
+        uint pgia = 0;
+ 		char bulled;
+ 		bool count[guess.length()];
+ 		for(uint i=0;i<guess.length();i++){
+ 			count[i]=false;
+ 		}
 
-  //       for(int i = 0; i<choosen.length(); i++){
-  //       	bool flag=false;
-		// 		if(choosen[i]==guess[i]){
-		// 			bull++;
-		// 			flag=true;
-		// 			bulled=choosen[i];
-		// 		}
+        for(int i = 0; i<choosen.length(); i++){
+        	bool flag=false;
+				if(choosen[i]==guess[i]){
+					bull++;
+					flag=true;
+					bulled=choosen[i];
+				}
 
-		// 		if(!flag){
-  //               for(int j = 0; j<guess.length(); j++){
-  //               	if(i!=j){
-  //                   if(choosen[i]==guess[j]&&choosen[i]!=bulled){
-  //                   	if(count[j]!=true){
-  //                           pgia++;
-  //                           count[j]=true;
-  //                           break;
-  //                       }
-  //                           }
-  //                   			}
-  //               	}
-  //               }
-  //           }
-  //       	std::string s=(to_string(bull)+","+to_string(pgia));
-  //        return s;
-  //   }
-		return "";
+				if(!flag){
+                for(int j = 0; j<guess.length(); j++){
+                	if(i!=j){
+                    if(choosen[i]==guess[j]&&choosen[i]!=bulled){
+                    	if(count[j]!=true){
+                            pgia++;
+                            count[j]=true;
+                            break;
+                        }
+                            }
+                    			}
+                	}
+                }
+            }
+        	std::string s=(to_string(bull)+","+to_string(pgia));
+         return s;
+    }
 	}
-// 		void check(string a, string b){
-//         string nully= "";
+		void check(string a, string b){
+        string nully= "";
 
-// 			if(a.compare(nully)==0||b.compare(nully)==0)
-// 				throw invalid_argument("Error: empty!");
+			if(a.compare(nully)==0||b.compare(nully)==0)
+				throw invalid_argument("Error: empty!");
 
-// 			for(uint i=0;i<a.length();i++){
-// 			if(b.at(i)<'0'||b.at(i)>'9')
-// 				throw invalid_argument("Error: empty!");
+			for(uint i=0;i<a.length();i++){
+			if(b.at(i)<'0'||b.at(i)>'9')
+				throw invalid_argument("Error: empty!");
 
-// 			if(a[i]<'0'||a[i]>'9'){
-// 				throw std::invalid_argument("Error: not a number!");
-// 			}
-// 		}
-// 	}
+			if(a[i]<'0'||a[i]>'9'){
+				throw std::invalid_argument("Error: not a number!");
+			}
+		}
+	}
 	}
