17cdbf52e522c40f504b4f2f83ad626dfbc3c01c
Michael-Lemberger <44966756+michael-lemberger@users.noreply.github.com>
2019-05-07 12:46:24 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -9,14 +9,20 @@ namespace bullpgia {
 
         uint bull = 0;
         uint pgia = 0;
+		uint length= guess.length();
+		if(length>choosen.length())
+			length=choosen.length();
  		bool f_guess[guess.length()];
- 		bool f_chooser[guess.length()];
- 		for(uint i=0;i<guess.length();i++){
- 			f_chooser[i]=false;
+ 		bool f_chooser[choosen.length()];
+		
+ 		for(uint i=0;i<choosen.length();i++){
+ 			f_guess[i]=false;
+ 		}
+		for(uint i=0;i<guess.length();i++){
  			f_guess[i]=false;
  		}
 
- 		for(int i = 0; i<choosen.length(); i++){
+ 		for(uint i = 0; i<legnth; i++){
  			if(choosen[i]==guess[i]){
  				f_chooser[i]=true;
  				f_guess[i]=true;
