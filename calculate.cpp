dd7628251f64985f59a7a48e52602efe43fbf912
Michael-Lemberger <44966756+michael-lemberger@users.noreply.github.com>
2019-05-07 12:11:57 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -14,37 +14,38 @@ namespace bullpgia {
     	check(choosen,guess);
         uint bull = 0;
         uint pgia = 0;
- 		char bulled;
- 		bool count[guess.length()];
+ 		bool f_guess[guess.length()];
+ 		bool f_chooser[guess.length()];
  		for(uint i=0;i<guess.length();i++){
- 			count[i]=false;
+ 			f_chooser[i]=false;
+ 			f_guess[i]=false;
+ 		}
+
+ 		for(int i = 0; i<choosen.length(); i++){
+ 			if(choosen[i]==guess[i]){
+ 				f_chooser[i]=true;
+ 				f_guess[i]=true;
+ 				bull++;
+ 			}
  		}
 
         for(int i = 0; i<choosen.length(); i++){
-        	bool flag=false;
-				if(choosen[i]==guess[i]){
-					bull++;
-					flag=true;
-					bulled=choosen[i];
-				}
-
-				if(!flag){
+        	if(f_chooser[i]!=true){
                 for(int j = 0; j<guess.length(); j++){
-                	if(i!=j){
-                    if(choosen[i]==guess[j]&&choosen[i]!=bulled){
-                    	if(count[j]!=true){
+                    if(choosen[i]==guess[j]){
+                    	if(f_guess[j]!=true){
                             pgia++;
-                            count[j]=true;
+                            f_guess[j]=true;
                             break;
-                        }
-                            }
-                    			}
-                	}
-                }
+        	}
             }
-        	string s=(to_string(bull)+","+to_string(pgia));
+        }
+    }
+    }
+        	std::string s=(to_string(bull)+","+to_string(pgia));
          return s;
     }
+		return "";
 	}
 		void check(string a, string b){
         string nully= "";
@@ -54,10 +55,10 @@ namespace bullpgia {
 
 			for(uint i=0;i<a.length();i++){
 			if(b.at(i)<'0'||b.at(i)>'9')
-				throw invalid_argument("Error: not a number!");
+				throw invalid_argument("Error: empty!");
 
 			if(a[i]<'0'||a[i]>'9'){
-				throw invalid_argument("Error: not a number!");
+				throw std::invalid_argument("Error: not a number!");
 			}
 		}
 	}
